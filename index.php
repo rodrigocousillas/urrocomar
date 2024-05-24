c673abdd396e61508c90e06125b895398b3b060a
Rodrigo Cousillas <cousillas.rodrigo@gmail.com>
2024-05-24 19:36:59 -0300

    Primer subida

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Urro / Cou & Cou</title>
+    <meta name="description" content="Laboratorio de pruebas y ensayos tecnologicos de la Agencia de Desarrollo Cou & Cou.">
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  
+	<div class="fondo">
+		<div class="container1">
+			<!-- <video src="video.webm" poster="presentacion.jpg" controls></video> -->
+            <!-- Ejemplo 2 -->
+            
+            <video class="desktop video" src="video/infinitos.mp4" autoplay muted loop></video>
+
+            <div class="mobile">
+                <video width="90%" autoplay muted loop>
+                    <source src="video/infinito.mp4" type="video/mp4" />
+                </video>    
+            </div>
+            <p><strong>Infinity</strong> is a great place to <span style="font-weight: 500; font-size: 30px;">start</span></p>
+           
+		</div>
+	</div>
+
+</body>
+</html>
